7c7774234554629ece0db32db950afbdabb74e40
marcus pedersen <marcus@mpedersen.me>
2024-09-03 16:29:09 +0200

    Start to implement MovieController and MovieDto

---
+++ Movies.API/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        
+        public ActionResult<string> GetMovies()
+        {
+            return Ok("Getting movies");
+        }
+    }
+}
---
+++ Movies.API/Models/DTO/MovieDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Movies.API.Models.DTO
+{
+    public record MovieDto(
+        int Id,
+        string Title,
+        long Rating,
+        DateTime ReleaseDate,
+        string Description
+    );
+}
